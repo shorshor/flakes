d3518b8e98ae766716190e13263a8b1d76cb87ab
Clydene Desiree Burgesson <132778800+shorshor@users.noreply.github.com>
2023-07-01 22:55:59 +0000

    slightly more  edits

--- flake.nix
+++ flake.nix
@@ -1,15 +1,44 @@
-{ pkgs }:
-
 {
-  elixir = pkgs.mkShell {
-    name = "elixir-env";
-    
-    packages = with pkgs; [ elixir livebook postgresql ];
+  description = "Elixir, Livebook, and Postgres development environment";
 
-    shellHook = ''
-      echo "Welcome to a Nix development environment for Elixir, Livebook, and Postgres!"
-      mix local.hex --if-missing --force
-      mix archive.install hex phx_new --force
-    '';
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
   };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux"   # 64-bit Intel/AMD Linux
+        "aarch64-linux"  # 64-bit ARM Linux
+        "x86_64-darwin"  # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = with pkgs; [
+            elixir
+            livebook
+            postgresql
+          ];
+
+          shellHook = ''
+            echo "Welcome to a Nix development environment for Elixir, Livebook, and Postgres!"
+            mix local.hex --if-missing --force
+            mix archive.install hex phx_new --force
+          '';
+        };
+      });
+    };
 }
