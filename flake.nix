08b418b061d86858a9dd52aeb97c8bfdcd95acf1
Clydene Desiree Burgesson <132778800+shorshor@users.noreply.github.com>
2023-07-01 22:51:04 +0000

    slight edits

--- flake.nix
+++ flake.nix
@@ -4,40 +4,10 @@
   elixir = pkgs.mkShell {
     name = "elixir-env";
     
-    packages = with pkgs; [ elixir ];
+    packages = with pkgs; [ elixir livebook postgresql ];
 
     shellHook = ''
-      echo "Welcome to a Nix development environment for Elixir!"
-    '';
-  };
-
-  livebook = pkgs.mkShell {
-    name = "livebook-env";
-    
-    packages = with pkgs; [ livebook ];
-
-    shellHook = ''
-      echo "Welcome to a Nix development environment for Livebook!"
-    '';
-  };
-
-  postgres = pkgs.mkShell {
-    name = "postgres-env";
-    
-    packages = with pkgs; [ postgresql ];
-
-    shellHook = ''
-      echo "Welcome to a Nix development environment for Postgres!"
-    '';
-  };
-
-  phoenix = pkgs.mkShell {
-    name = "phoenix-env";
-    
-    packages = with pkgs; [ elixir ];
-
-    shellHook = ''
-      echo "Welcome to a Nix development environment for Phoenix!"
+      echo "Welcome to a Nix development environment for Elixir, Livebook, and Postgres!"
       mix local.hex --if-missing --force
       mix archive.install hex phx_new --force
     '';
